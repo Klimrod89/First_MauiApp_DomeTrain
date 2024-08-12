ee42fd2741d80a12e8d10e7078cb191ecd60270a
On Ymac Windows <koutoukale+VSGit@gmail.com>
2024-08-12 19:14:54 -0400

    First commit

---
+++ HelloMaui/MainPage.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Maui.Markup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloMaui;
+public class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        Content = new Label()
+            .Text("This is a Maui Label!")
+            .Center()
+            .TextCenter();
+    }
+}
