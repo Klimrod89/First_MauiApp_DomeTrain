ded885151a9fb966306be83455508cc7c09114ee
Yassir Chitou <koutoukale+git@gmail.com>
2024-08-12 20:03:42 -0400

    Working on it

---
+++ HelloMaui/BaseContentPage.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Maui.Controls.PlatformConfiguration;
+using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloMaui;
+public abstract class BaseContentPage: ContentPage
+{
+    public BaseContentPage()
+    {
+        On<iOS>().SetUseSafeArea(true);
+    }
+}
--- HelloMaui/MainPage.cs
+++ HelloMaui/MainPage.cs
@@ -6,13 +6,26 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace HelloMaui;
-public class MainPage : ContentPage
+public class MainPage : BaseContentPage
 {
     public MainPage()
     {
-        Content = new Label()
-            .Text("This is a Maui Label!")
-            .Center()
-            .TextCenter();
+        Content = new VerticalStackLayout
+        {
+            Children =
+            {
+                new Image()
+                .Size(500 , 250)
+                .Aspect(Aspect.AspectFit)
+                .Source("appicon"),
+                new Label()
+                .Text("Hello MAUI", Colors.Black)
+                .Center()
+                .TextCenter(),
+                new Entry()
+                .Placeholder("Notes", Colors.DarkGray)
+                .TextColor(Colors.Black)
+            }
+        };
     }
 }
